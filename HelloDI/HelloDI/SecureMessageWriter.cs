aa7dcd62d34057f5aaacfe24c53adfe298e79e28
Yuri Partitski <mrblondy97@yandex.ru>
2017-10-20 13:45:46 +0200

    update

---
+++ HelloDI/HelloDI/SecureMessageWriter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloDI
+{
+	public class SecureMessageWriter : IMessageWriter
+	{
+		private readonly IMessageWriter writer;
+		public SecureMessageWriter(IMessageWriter writer)
+		{
+			if (writer == null)
+			{
+				throw new ArgumentNullException("writer");
+			}
+			this.writer = writer;
+		}
+		public void Write(string message)
+		{
+			if (Thread.CurrentPrincipal.Identity
+			.IsAuthenticated)
+			{
+				this.writer.Write(message);
+			}
+		}
+	}
+}
